8e89c5dedbd817a5988436e61334d5d13cd2b4e8
Darkpandarts <114529342+darkpandarts@users.noreply.github.com>
2023-12-06 03:59:19 +0000

    seetup deep source coverage analysin in GHA

--- deepsource_pytest.sh
+++ deepsource_pytest.sh
@@ -13,7 +13,7 @@ curl https://deepsource.io/cli | sh
 
 # Set DEEPSOURCE_DSN env variable from repository settings page
 # must be exported befoire
-export DEEPSOURCE_DSN=${PRIVATE_REPO_DSN}
+export DEEPSOURCE_DSN=${DEEPSOURCE_BUILD_REPO_DSN}
 
 # From the root directory, run the report coverage command
 ./bin/deepsource report --analyzer test-coverage --key python --value-file ./coverage.xml
