4b28908e3d6e51cb70c2b6e15b77891a6557c883
Darkpandarts <114529342+darkpandarts@users.noreply.github.com>
2023-12-11 00:44:09 +0000

    make progress on replaing rst --> md TOC 1

--- scrap_toc.py
+++ scrap_toc.py
@@ -14,59 +14,87 @@ from bashautodoc.models.filepath_datahandler import FilepathDatahandler
 def extract_lines_between_tags(filetext):
     line_holder = []
     inRecordingMode = False
-    blank_linecount = 0
     for line in filetext.split("\n"):
-        line_stripped = line.strip()
-        # print("line", line)
+        # line_stripped = line.strip()
         if not inRecordingMode:
             if "```{toctree}" in line:
                 rprint("TRUE: found toctree")
                 inRecordingMode = True
-                # line_holder.append(line)
-        ### Recording stops if 2 blank lines are encountered
-        ### or naturally at the end of the file
-        elif line_stripped == "```":
-            # blank_linecount += 1
-            # if blank_linecount == 2:
-            #     blank_linecount = 0
-            # inRecordingMode = False
+                line_holder.append(line)
+        elif "```" in line:
             inRecordingMode = False
+            line_holder.append(line)
         else:
-            if (line_stripped != "") and "maxdepth:" not in line_stripped:
-                line_holder.append(line_stripped)
+            line_holder.append(line)
 
     return line_holder
 
 
 ###########
-hwdoc_relpaths = hfile.search_directory_with_multiple_globs(
-    search_path="./docs_bash-it/docs/docshw/",
-    glob_patt_list=["*.md"],
-)
+if __name__ == "__main__":
+    hwdoc_relpaths = hfile.search_directory_with_multiple_globs(
+        search_path="./docs_bash-it/docs/docshw/",
+        glob_patt_list=["*.md"],
+    )
 
-rprint("hwdoc_relpaths: %s", hwdoc_relpaths)
+    rprint("hwdoc_relpaths: %s", hwdoc_relpaths)
 
-
-for hwdoc_relpath in hwdoc_relpaths:
-    rprint("\nhwdoc_relpath", hwdoc_relpath)
-    hwdoc_root, hwdoc_extension = hwdoc_relpath.rsplit("/", 1)
-    rprint("\nhwdoc_root", hwdoc_root, hwdoc_extension)
-    # sys.exit(42)
-    if "index" in hwdoc_relpath:
+    for hwdoc_relpath in hwdoc_relpaths:
         rprint("\nhwdoc_relpath", hwdoc_relpath)
-        filetext = hfile.read_file_2string(filepath=hwdoc_relpath)
-        # rprint("\nfiletext: %s", filetext)
+        hwdoc_root, hwdoc_extension = hwdoc_relpath.rsplit("/", 1)
+        rprint("\nhwdoc_root", hwdoc_root, hwdoc_extension)
+        # sys.exit(42)
+        if "index" in hwdoc_relpath:
+            rprint("\nhwdoc_relpath", hwdoc_relpath)
+            filetext = hfile.read_file_2string(filepath=hwdoc_relpath)
+            # rprint("\nfiletext: %s", filetext)
 
-        rprint("\n\n")
+            rprint("\n\n")
 
-        toc_list = extract_lines_between_tags(filetext=filetext)
-        rprint("\ntoc_list: %s", toc_list)
+            toc_list = extract_lines_between_tags(filetext=filetext)
+            rprint("\ntoc_list: %s", toc_list)
 
-        for item in toc_list:
-            file_link_list = hfile.list_matching_files_recursively(
-                search_path=hwdoc_root,
-                myglob=f"{item}.md",
+            toc_list_cleaned = [
+                line
+                for line in toc_list
+                if line != "" and "maxdepth:" not in line and "```" not in line
+            ]
+
+            print("toc_list_cleaned", toc_list_cleaned)
+            # sys.exit(42)
+
+            md_toclink_list = []
+            for toc_linkname in toc_list_cleaned:
+                file_link_list = hfile.list_matching_files_recursively(
+                    search_path=hwdoc_root,
+                    myglob=f"{toc_linkname}.md",
+                )
+                rprint("\nfile_link_list", f"{toc_linkname}.md", file_link_list)
+
+                if len(file_link_list) > 1:
+                    print("ERROR: more than one file found")
+                    sys.exit(42)
+
+                md_rellink_cleaned = file_link_list[0]  # .replace("./", "")
+                md_rellink = f"- [**{toc_linkname.capitalize().replace('/index', '')}**]({md_rellink_cleaned})"
+                rprint(
+                    "md_rellink",
+                )
+                md_toclink_list.append(md_rellink)
+
+            print("md_toclink_list", md_toclink_list)
+
+            joined_original_toclinks = "\n".join(toc_list)
+            rprint("\njoined_original_toclinks\n", joined_original_toclinks)
+
+            joined_toclinks = "\n".join(md_toclink_list)
+            rprint("\njoined_toclinks\n", joined_toclinks)
+            # sys.exit(42)
+
+            print("\n\n")
+
+            replaced_filetext = filetext.replace(
+                joined_original_toclinks, joined_toclinks
             )
-            rprint("\nfile_link_list", f"{item}.md", file_link_list)
-
-        sys.exit(42)
+            print("replaced_filetext\n", replaced_filetext)
+            sys.exit(42)
