cd1a9755511baaea4f782bc91975221223935c62
Darkpandarts <114529342+darkpandarts@users.noreply.github.com>
2024-01-05 08:40:27 +0000

    horrorshow navbar restructure 5

---
+++ bashautodoc/gen_mkdocs_nav_bar.py
@@ -0,0 +1,175 @@
+import logging
+import os
+import sys
+
+from rich import print as rprint
+
+from bashautodoc.helpo.hfile import (
+    copy_dir,
+    mkdir_if_notexists,
+    move_files_and_dirs,
+    rmdir_if_exists,
+    write_dict_2yaml_file,
+)
+
+LOG = logging.getLogger(__name__)
+
+
+class GenMkdocsNavBar:
+    def __new__(cls, cnf, catname_2mdfile_dict, navbar_cleaned_dict):
+        cls.cnf = cnf
+        cls.catname_2mdfile_dict = catname_2mdfile_dict
+        cls.navbar_cleaned_dict = navbar_cleaned_dict
+        cls.navdict = {"nav": []}
+
+        return cls.main()
+
+    @classmethod
+    def mkdocs_add_handwrittendocs_to_nav(cls):
+        cls.navdict.update(cls.navbar_cleaned_dict)
+        rprint("cls.navdict", cls.navdict)
+        # sys.exit(42)
+
+        rprint("\n\ncls.navdict['nav']", cls.navdict["nav"])
+        # sys.exit(42)
+
+        # TODO: fix hacky copy/replace docs dir
+
+        # sys.exit(42)
+        rmdir_if_exists(target="docs_bash-it/docs_temp")
+        move_files_and_dirs(
+            source="docs_bash-it/docs/docshw", target="docs_bash-it/docs_temp"
+        )
+        move_files_and_dirs(
+            source="docs_bash-it/docs/custom_css", target="docs_bash-it/custom_css_temp"
+        )
+        # sys.exit(42)
+        rmdir_if_exists(target="docs_bash-it/docs")
+        move_files_and_dirs(source="docs_bash-it/docs_temp", target="docs_bash-it/docs")
+        move_files_and_dirs(
+            source="docs_bash-it/custom_css_temp", target="docs_bash-it/docs/custom_css"
+        )
+
+        # sys.exit(42)
+
+        # for catname in ["docshw"]:  # cls.classmethod"catnames_docs"):
+        #     LOG.debug("catname: %s", catname)
+
+        #     cat_mdoutfiles_rpaths = sorted(catname_2mdfile_dict.get(catname))
+        #     catname_holder = []
+        #     for mdoutfile_rpath in cat_mdoutfiles_rpaths:
+        #         print("catname", catname)
+        #         print("mdoutfile_rpath", mdoutfile_rpath)
+        #         page_name = mdoutfile_rpath.replace(".md", "").split("/")[-1]
+        #         mdoutfile_routepath = mdoutfile_rpath.replace(
+        #             f"{cls.cnf.project_docs_dir}", "."
+        #         )
+        #         print("cls.cnf.project_docs_dir", cls.cnf.project_docs_dir)
+        #         print("mdoutfile_routepath", mdoutfile_routepath)
+        #         # sys.exit(42)
+        #         page_path_map = {page_name: mdoutfile_routepath}
+        #         catname_holder.append(page_path_map)
+
+        # ###########################################
+        # srcdocs_parent = None
+        # if srcdocs_parent is None:
+        #     cls.yaml_dict["nav"].append({catname: catname_holder})
+        # else:
+        #     cls.yaml_dict["nav"][1][srcdocs_parent].append(
+        #         {catname: catname_holder}
+        #     )
+
+        # (
+        #     docfile_path_split,
+        #     docoutdir_rpath,
+        #     docfilename,
+        # ) = get_src_reldir_and_filename(
+        #     file_rpath=docfile_rpath,
+        #     glob_patterns=cls.classmethod"docs_glob_patterns"),
+        #     replace_str=".md",
+        # )
+
+        # LOG.debug("docfilename: %s", docfilename)
+        # LOG.debug("docfilename_noext: %s", docfilename_noext)
+        # sys.exit(42)
+
+        # # TODOD: change docfilename --> docfilename_noext
+        # cls.yaml_dict["nav"].append({docfilename: docfile_rpath})
+
+    @classmethod
+    def mkdocs_add_srcdocs_to_nav(cls):
+        rprint("catname_2mdfile_dict", cls.catname_2mdfile_dict)
+        # sys.exit(42)
+
+        # TODO: re-enable optional srcdocs nesting once things are refactored
+        # ref_or_main_raw = cls.classmethod"nav_codedocs_as_ref_or_main")
+        # ref_or_main = ref_or_main_raw if ref_or_main_raw else "main"
+
+        # nav_codedocs_name_raw = cls.classmethod"nav_codedocs_name")
+        # nav_codedocs_name = (
+        #     nav_codedocs_name_raw if cls.classmethod"nav_codedocs_name") else "Code-Docs"
+        # )
+
+        # if ref_or_main == "main":
+        #     srcdocs_parent = None
+        # elif ref_or_main == "ref":
+        #     cls.yaml_dict["nav"].append({nav_codedocs_name: []})
+        #     srcdocs_parent = nav_codedocs_name
+        # else:
+        #     LOG.error(
+        #         "Error: nav_codedocs_as_ref_or_main must be set to either 'main' or 'ref'"
+        #     )
+        #     sys.exit(42)
+
+        LOG.info("Add generated code docs to nav")
+        rprint("catname_2mdfile_dict", sorted(cls.catname_2mdfile_dict["undef"]))
+        # sys.exit(42)
+
+        srcdoc_dict = {"nav": [{"Reference": []}]}
+        for catname in cls.cnf.catnames_src:
+            print("catname", catname)
+            cat_mdoutfiles_rpaths = sorted(cls.catname_2mdfile_dict.get(catname))
+            catname_holder = []
+
+            for mdoutfile_rpath in cat_mdoutfiles_rpaths:
+                print("catname", catname)
+                print("mdoutfile_rpath", mdoutfile_rpath)
+                page_name = mdoutfile_rpath.replace(".md", "").split("/")[-1]
+                mdoutfile_routepath = mdoutfile_rpath.replace(
+                    f"{cls.cnf.project_docs_dir}", "."
+                )
+                print("cls.cnf.project_docs_dir", cls.cnf.project_docs_dir)
+                print("mdoutfile_routepath", mdoutfile_routepath)
+                # sys.exit(42)
+                page_path_map = {page_name: mdoutfile_routepath}
+                catname_holder.append(page_path_map)
+
+            # rprint("cls.yaml_dict[", cls.yaml_dict)
+            # if srcdocs_parent is None:
+            #     cls.yaml_dict["nav"].append({catname: catname_holder})
+            # else:
+            #     cls.yaml_dict["nav"][1][srcdocs_parent].append(
+            #         {catname: catname_holder}
+            #     )
+            srcdoc_dict["nav"].append({catname: catname_holder})
+
+        rprint("\n\navdict", cls.navdict)
+        rprint("\n\nsrcdoc_dict", srcdoc_dict)
+
+        cls.navdict.update(srcdoc_dict)
+
+        rprint("\n\nnavdict2", cls.navdict)
+        # sys.exit(42)
+
+    @classmethod
+    def main(cls):
+        if cls.cnf["handwritten_docs_dir"]:
+            LOG.info("Set handwritten docs as main to nav")
+            cls.mkdocs_add_handwrittendocs_to_nav()
+
+        # LOG.info("Set generated src docs to nav")
+        # cls.mkdocs_add_srcdocs_to_nav()
+
+        rprint("cls.navdict", cls.navdict)
+
+        return cls.navdict
---
+++ bashautodoc/gen_navbar_dict.py
@@ -0,0 +1,219 @@
+import logging
+import sys
+from collections import defaultdict
+
+from rich import print as rprint
+
+from bashautodoc.helpo import hfile
+from bashautodoc.helpo.hstrops import (
+    clean_str_pline,
+    get_lines_between_tag_and_blank_line,
+)
+from bashautodoc.regex_patterns import *
+
+LOG = logging.getLogger(__name__)
+
+# TODO: initialise all regex patterns in a single place
+
+
+# TODO: refactor the hell out of this ;o)
+# TODO: sort out conversion of relative paths sooner
+class GenNavbarDict:
+    def __init__(self, cnf, search_path) -> None:
+        ### For every mdfile
+        # 1. establish if it has a TOC
+        # 2. Map the hierarchy of nav-doc links via the TOC
+        # 3. We will use the anchor "## Table of Contents" to find md TOC
+
+        # TODO: bad var name
+        self.project_docs_dir_local = cnf.project_docs_dir + "/"
+        LOG.debug("project_docs_dir: %s", self.project_docs_dir_local)
+        # sys.exit(42)
+        #
+        self.toc_dict = {}
+        self.hierarchy_dict = defaultdict(list)
+        self.navbar_dict = {}
+
+        self.mdtoc_path_list = hfile.flatten_list(
+            nested_list=hfile.find_files_with_grep_patt(
+                search_path=search_path,
+                file_glob="*.md",
+                txt_pattern="## Table of Contents",
+            )
+        )
+        rprint("mdtoc_path_list", self.mdtoc_path_list)
+
+    @staticmethod
+    def clean_mdtoc_list(toc_mdlist):
+        clean_toc_mdlist = []
+        for line in toc_mdlist:
+            line_stripped = line.strip()
+            if "## Table of Contents" in line_stripped:
+                pass
+            elif line_stripped == "":
+                pass
+            else:
+                mdlink_match = mdlink_patt.search(line_stripped)
+                if mdlink_match is not None:
+                    mdlink = mdlink_match.group(2)
+                    clean_toc_mdlist.append(mdlink)
+        return clean_toc_mdlist
+
+    def gen_toc_dict_from_mdindex_files(self):
+        for mdpath in self.mdtoc_path_list:
+            mdpath_rel = mdpath.replace(self.project_docs_dir_local, "")
+            file_text = hfile.read_file_2string(filepath=mdpath)
+            table_of_contents = get_lines_between_tag_and_blank_line(
+                file_text, start_tag="## Table of Contents"
+            )
+            self.toc_dict[mdpath_rel] = GenNavbarDict.clean_mdtoc_list(
+                toc_mdlist=table_of_contents
+            )
+        rprint("\ntoc_dict", self.toc_dict)
+        # sys.exit(42)
+
+    def order_indexes_in_toc_dict(self):
+        # copy index files to front of list
+        # catnames_list = list(self.toc_dict.keys())
+        for mdpath, toc_links in self.toc_dict.items():
+            rprint("mdpath", mdpath)
+            # rprint("toc_links", toc_links)
+            # sys.exit(42)
+            toc_links_with_index = []
+            if mdpath == "docshw/index.md":
+                # TODO: fix this hard coded hack - think we need to specify main index in config
+                continue
+            for mdlink in toc_links:
+                if mdpath != mdlink:
+                    rprint("mdlink", mdlink)
+                    ### deletes the index.mdfile from the list
+                    # (as we want to make this the forst element in the list)
+                    toc_links_with_index.append(mdlink)
+            toc_links_with_index.insert(
+                0, mdpath
+            )  ### inserts the index.mdfile at the start of the list
+            self.toc_dict[mdpath] = toc_links_with_index
+        rprint("toc_dict index ordered", self.toc_dict)
+
+    def construct_hierarchy_dict(self):
+        for mdpath, _ in self.toc_dict.items():
+            rprint("mdpath", mdpath)
+            for file_path2, toc_links2 in self.toc_dict.items():
+                rprint("file_path2", file_path2)
+                mdpath_rel = mdpath.replace(self.project_docs_dir_local, "")
+                if mdpath_rel in toc_links2:
+                    self.hierarchy_dict[file_path2].append(mdpath_rel)
+                    break
+        rprint("\nhierarchy_dict", self.hierarchy_dict)
+
+    @staticmethod
+    def gen_toc_kvdict(mylink, myvalue):
+        """
+        This function splits the category and returns the toc key and value.
+        """
+        tockey = clean_str_pline(mylink.split("/")[-1], [".md"])
+        tocvalue = clean_str_pline(mylink, [myvalue])
+
+        # if "misc" in tockey:
+        #     print("tockey", tockey)
+        #     print("tocvalue", tocvalue)
+        #     sys.exit(42)
+        return {tockey: tocvalue}
+
+    def construct_navbar_dict(self):
+        for key, link_list in sorted(
+            self.hierarchy_dict.items(), key=lambda x: len(x[1]), reverse=True
+        ):
+            rprint("link_list", link_list)
+
+            self.navbar_dict[key] = self.toc_dict[key]
+            rprint("navbar_dict", self.navbar_dict)
+            # sys.exit(42)
+
+            new_link_list = []
+            for mdlink in self.navbar_dict[key]:
+                if mdlink in link_list:
+                    rprint("mdlink", mdlink)
+                    # sys.exit(42)
+                    category_split = mdlink.split("/")
+                    if category_split[-1] == "index.md":
+                        category_name = category_split[-2]
+                    else:
+                        category_name = category_split[-1]
+
+                    yaml2_sublist = [
+                        GenNavbarDict.gen_toc_kvdict(
+                            mylink=link, myvalue=self.project_docs_dir_local
+                        )
+                        for link in self.toc_dict[mdlink]
+                    ]
+
+                    sub_dict = {category_name: yaml2_sublist}
+                    new_link_list.append(sub_dict)
+                else:
+                    toc_kvdict = GenNavbarDict.gen_toc_kvdict(
+                        mylink=mdlink, myvalue=self.project_docs_dir_local
+                    )
+                    new_link_list.append(toc_kvdict)
+            self.navbar_dict[key] = new_link_list
+        rprint("navbar_dict", self.navbar_dict)
+
+    def main(self):
+        self.gen_toc_dict_from_mdindex_files()
+        # sys.exit(42)
+
+        ## 1. Check which mdtoc_path is in which list to figure out rough order
+        ## 2. If it is in the list, then it is suboridnate
+        ## 3. Create ranked order of mdfiles
+        self.construct_hierarchy_dict()
+        # sys.exit(42)
+
+        self.order_indexes_in_toc_dict()
+        # sys.exit(42)
+
+        self.construct_navbar_dict()
+        sys.exit(42)
+
+        #####################
+        walk_nested_dicts_with_lists(obj=self.navbar_dict)
+
+        print("#####################")
+        self.navbar_cleaned_dict = {"nav": []}
+        for key, values in self.navbar_dict.items():
+            if key == "docshw/index.md":
+                key_home = "Home"
+                self.navbar_cleaned_dict["nav"].append({key_home: "index.md"})
+                if isinstance(values, list):
+                    for kvdict in values:
+                        for k, v in kvdict.items():
+                            self.navbar_cleaned_dict["nav"].append({k: v})
+
+            else:
+                key_renamed = key.replace("docshw/", "")
+                self.navbar_cleaned_dict["nav"].append({key_renamed: values})
+
+        for key, values in self.navbar_cleaned_dict.items():
+            rprint("**", key, values)
+
+        rprint("\nnavbar_cleaned_dict", self.navbar_cleaned_dict)
+        # sys.exit(42)
+
+        return self.navbar_cleaned_dict
+
+
+# TODO: rename this function and put it somewhere more appropriate
+def walk_nested_dicts_with_lists(obj):
+    if isinstance(obj, list):  # could replace with collections.abc.MutableSequence
+        myiterable = enumerate(obj)
+    elif isinstance(obj, dict):  # could replace with collections.abc.MutableMapping
+        myiterable = obj.items()
+    else:
+        return  # don't iterate -- pass back up
+
+    for key, value in myiterable:
+        if isinstance(value, str):
+            # TODO: allow custiom function to be passed in using partial
+            obj[key] = value.replace("docshw/", "")
+        else:
+            walk_nested_dicts_with_lists(value)
+    return obj
